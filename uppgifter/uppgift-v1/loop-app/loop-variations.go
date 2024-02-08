ec291029b2a719b2dfa13c5a817653ec04eca3e0
Andrés Martinez-Losa <asvimalo@gmail.com>
2024-02-08 10:55:52 +0100

    loopbetween created

--- uppgifter/uppgift-v1/loop-app/loop-variations.go
+++ uppgifter/uppgift-v1/loop-app/loop-variations.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+    "os/exec"
 )
 
 func loopdecimal() {
@@ -32,10 +34,47 @@ func loopbetween() {
 		swap = num1
 		num1 = num2
 		num2 = swap
-	} 
+	}
 	num1++
 	for num1 < num2 {
-		fmt.Println(num1)	
+		fmt.Println(num1)
 		num1++
 	}
+}
+
+func minicalculator() {
+	/*
+			Skapa ett program där användaren
+
+		    Får mata in två tal.
+		    Skriv sedan till skärmen summan av de två talen.
+		    Skriv sedan en fråga- Vill du fortsätta(J/N)?.
+		    Svarar användaren J återupprepas punkt a-c
+		    Svarar användaren N avbryts programmet
+
+	*/
+	var number1 int
+	var number2 int
+	//var answer rune
+	var answer string
+
+	for {
+		cmd := exec.Command("clear")
+    	cmd.Stdout = os.Stdout
+    	cmd.Run()
+		fmt.Print("Enter first number: ")
+		fmt.Scan(&number1)
+		fmt.Print("Enter second number: ")
+		fmt.Scan(&number2)
+
+		fmt.Printf("Sum = %d\n", number1+number2)
+
+		fmt.Print("Do you want to continue (Y/N)?: ")
+		fmt.Scan(&answer)
+
+		if answer == "N" {
+			break
+		}
+	}
+
 }
